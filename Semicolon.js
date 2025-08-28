310e64cba3b877608ae3bfc0708c307937ab8ec6
Indah135 <indahamiati85@gmail.com>
2025-08-28 15:46:17 +0700

    update

--- Semicolon.js
+++ Semicolon.js
@@ -1,5 +1,6 @@
 const text = `"Your story isn’t over yet; the semicolon reminds us to keep going."`;
 let index = 0;
+
 function typeEffect() {
   if (index < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(index);
@@ -8,3 +9,14 @@ function typeEffect() {
   }
 }
 typeEffect();
+
+document.querySelector(".show-image").addEventListener("click", function(e) {
+  e.preventDefault(); 
+  const imgContainer = document.getElementById("image-container");
+
+  if (imgContainer.style.display === "none" || imgContainer.style.display === "") {
+    imgContainer.style.display = "block";
+  } else {
+    imgContainer.style.display = "none";
+  }
+});
